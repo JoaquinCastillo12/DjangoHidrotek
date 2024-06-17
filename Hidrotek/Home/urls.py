97f6375668b796a3736c6881e1c6eefe24bc2a61
JoaquinC <joquin1254@gmail.com>
2024-06-17 10:17:56 -0500

    Login actualizado

--- Hidrotek/Home/urls.py
+++ Hidrotek/Home/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('category/<str:foo>', views.category, name='category'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
 ]
